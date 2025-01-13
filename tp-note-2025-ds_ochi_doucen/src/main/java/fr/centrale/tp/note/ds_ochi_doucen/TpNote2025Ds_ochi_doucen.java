caaf8cea4c0d8a063762d433272eec0e8fc0418c
Tatsuya0924 <wootatsuya0924@gmail.com>
2025-01-13 07:38:37 +0100

    First commit

---
+++ tp-note-2025-ds_ochi_doucen/src/main/java/fr/centrale/tp/note/ds_ochi_doucen/TpNote2025Ds_ochi_doucen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fr.centrale.tp.note.ds_ochi_doucen;
+
+/**
+ *
+ * @author woota
+ */
+public class TpNote2025Ds_ochi_doucen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

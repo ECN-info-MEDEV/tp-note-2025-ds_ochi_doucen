3e8ab8a6b92f543b374a931ce09332d1e6e116d4
Mathieu Doucen <mathieu.doucen@eleves.ec-nantes.fr>
2025-01-13 08:43:01 +0100

    classe plateau - fonctions afficher, placer et constructeur defaut.

---
+++ tp-note-2025-ds_ochi_doucen/src/main/java/fr/centrale/tp/note/ds_ochi_doucen/Joueur.java
@@ -0,0 +1,40 @@
+package fr.centrale.tp.note.ds_ochi_doucen;
+
+/**
+ *
+ * @author mathi & woota
+ */
+public class Joueur {
+    
+    //attributs
+    private String nom;
+    private int couleur;
+    boolean tour;
+    
+    //méthodes
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(int couleur) {
+        this.couleur = couleur;
+    }
+
+    public boolean isTour() {
+        return tour;
+    }
+
+    public void setTour(boolean tour) {
+        this.tour = tour;
+    }
+    
+}
